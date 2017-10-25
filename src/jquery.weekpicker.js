cb99a43d21b69ec8ede5c86cce278d3af86de73a
Mihai Marica <mihai.marica82@yahoo.com>
2017-10-25 22:15:58 +0300

    alter week line hover to not affect datepicker also

--- src/jquery.weekpicker.js
+++ src/jquery.weekpicker.js
@@ -1,45 +1,52 @@
 ;( function( $, window, document, undefined ) {
 
-	"use strict";
+    "use strict";
 
-		var pluginName = "WeekPicker",
-			defaults = {
-                firstDay: 1,
-                dateFormat: "dd/m/yy",
-                showOtherMonths: true,
-                selectOtherMonths: true,
-				showWeek: true
-            };
+    var pluginName = "WeekPicker",
+        defaults = {
+            firstDay: 1,
+            dateFormat: "dd/m/yy",
+            showOtherMonths: true,
+            selectOtherMonths: true,
+            showWeek: true
+        };
 
-		function WeekPicker ( element, options ) {
-			this.element = element;
+    function WeekPicker ( element, options ) {
+        this.element = element;
 
-			this.settings = $.extend( {}, defaults, options );
-			this.init();
-		}
+        this.settings = $.extend( {
+            beforeShow: this.beforeShow,
+            onClose: this.onClose
+        }, defaults, options );
 
-		$.extend( WeekPicker.prototype, {
-			init: function() {
-                $( "body" ).on( "mousemove", ".ui-weekpicker .ui-datepicker-calendar tr",
-                    function() { $( this ).find( "td a" ).addClass( "ui-state-hover" ); } );
-                $( "body" ).on( "mouseleave", ".ui-weekpicker .ui-datepicker-calendar tr",
-                    function() { $( this ).find( "td a" ).removeClass( "ui-state-hover" ); } );
+        return this.init();
+    }
 
-				this.yourOtherFunction();
-			},
-			yourOtherFunction: function() {
-                $( this.element ).datepicker( this.settings )
-					.datepicker( "widget" ).addClass( "ui-weekpicker" );
-			}
-		} );
+    $.extend( WeekPicker.prototype, {
+        init: function() {
+            $( this.element ).datepicker( this.settings );
 
-		$.fn.weekpicker = function( options ) {
-			return this.each( function() {
-				if ( !$.data( this, "plugin_" + pluginName ) ) {
-					$.data( this, "plugin_" +
-						pluginName, new WeekPicker( this, options ) );
-				}
-			} );
-		};
+            $( "body" ).on( "mousemove", ".ui-weekpicker .ui-datepicker-calendar tr",
+                function() { $( this ).find( "td a" ).addClass( "ui-state-hover" ); } );
+
+            $( "body" ).on( "mouseleave", ".ui-weekpicker .ui-datepicker-calendar tr",
+                function() { $( this ).find( "td a" ).removeClass( "ui-state-hover" ); } );
+        },
+        beforeShow: function() {
+            $( this ).datepicker( "widget" ).addClass( "ui-weekpicker" );
+        },
+        onClose: function() {
+            $( this ).datepicker( "widget" ).removeClass( "ui-weekpicker" );
+        }
+    } );
+
+    $.fn.weekpicker = function( options ) {
+        return this.each( function() {
+            if ( !$.data( this, "plugin_" + pluginName ) ) {
+                $.data( this, "plugin_" +
+                    pluginName, new WeekPicker( this, options ) );
+            }
+        } );
+    };
 
 } )( jQuery, window, document );
